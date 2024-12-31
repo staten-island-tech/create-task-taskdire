c19645747181c6e387e0745ec294e27b4d5035f3
taskdire <136046239+taskdire@users.noreply.github.com>
2024-12-30 23:51:28 -0500

    awd
    awd

--- app/blackjack.js
+++ app/blackjack.js
@@ -18,3 +18,29 @@ function getCardValue(card) {
   }
   return card.value;
 }
+
+
+/* 
+create 2 cards faced up on players side 
+
+ 2 cards on dealers side - 1 faced up - 1 faced down 
+
+ record values in a form 
+
+ wait for players respone 
+
+ make a hit and stand button 
+
+if hit add a card faced up on players side - record value -> if not over 21 ask player if they want to hit again - if over 21 "you busted" ->  wipe all cards on screen and repeat first 4 steps  
+ 
+if stand have both values compared and which ever one is the highest wins( if player value is higher "you win" if dealer higher "you lose") -> wipe all cards on screen and repeat first 4 steps
+
+
+
+
+
+
+
+
+*/
+
